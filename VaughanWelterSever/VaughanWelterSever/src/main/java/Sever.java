970184863ffa1d2f34fab81dfc709bb621a9b835
Anthony Welter <112017607+alw64@users.noreply.github.com>
2023-11-20 17:51:41 -0600

    Added server class

---
+++ VaughanWelterSever/VaughanWelterSever/src/main/java/Sever.java
@@ -0,0 +1,11 @@
+/****************************************************
+ 
+@author Dillon Vaughn, Anthony Welter
+File Name: Sever.java
+COP4027	Project #: 5
+
+****************************************************/
+
+public class Sever {
+
+}
