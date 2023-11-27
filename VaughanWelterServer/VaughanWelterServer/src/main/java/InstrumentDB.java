0209769230f44f9352aba5f6bced5605995e4bd2
Anthony Welter <112017607+alw64@users.noreply.github.com>
2023-11-26 19:01:17 -0600

    update directory

---
+++ VaughanWelterServer/VaughanWelterServer/src/main/java/InstrumentDB.java
@@ -0,0 +1,178 @@
+/****************************************************
+ 
+@author Dillon Vaughan, Anthony Welter
+File Name: InstrumentDB.java
+COP4027	Project #: 5
+
+****************************************************/
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class InstrumentDB {
+	Connection con;
+	Statement stat;
+	private Lock sqlAccessLock;
+   
+    /*
+	 * InstrumentDB constructor
+	 */
+	public InstrumentDB() throws Exception {
+		SimpleDataSource.init("database.properties");
+		con = SimpleDataSource.getConnection();
+		stat = con.createStatement();
+		sqlAccessLock = new ReentrantLock();
+
+		try {
+			sqlAccessLock.lock();
+			stat.execute("DROP TABLE Instruments");
+			stat.execute("DROP TABLE Locations");
+			stat.execute("DROP TABLE Inventory");
+			sqlAccessLock.unlock();
+		} 
+      catch (Exception e) {
+			System.err.println("Attempted to drop tables, tables did not exist!");
+		}
+
+		createInstruments(stat);
+		createLocations(stat);
+		createInventory(stat);
+	}
+   
+    /*
+	 * Drops tables and closes connection
+	 */
+	public void endDB() throws SQLException {
+		try {
+			sqlAccessLock.lock();
+			stat.execute("DROP TABLE Instruments");
+			stat.execute("DROP TABLE Locations");
+			stat.execute("DROP TABLE Inventory");
+		} catch (Exception e) {
+			System.err.println("Error, failed to drop tables!");
+		} finally {
+			sqlAccessLock.unlock();
+			con.close();
+			System.out.println("Dropped Tables, closed connection and ending program");
+		}
+	}
+   
+    /*
+	 * Creates table Instruments and returns table
+	 * @param Statement stat in order execute string based SQL string commands
+     * @return Instruments table
+	 */
+	public ResultSet createInstruments(Statement stat) throws Exception{	
+		 sqlAccessLock.lock();
+         stat.execute("CREATE TABLE Instruments (instName CHAR(12),instNumber INTEGER,cost DOUBLE,descrip CHAR(20))");
+         stat.execute("INSERT INTO Instruments VALUES ('guitar',1,100.0,'yamaha')");
+         stat.execute("INSERT INTO Instruments VALUES ('guitar',2,500.0,'gibson')");
+         stat.execute("INSERT INTO Instruments VALUES ('bass',3,250.0,'fender')");
+         stat.execute("INSERT INTO Instruments VALUES ('keyboard',4,600.0,'roland')");
+         stat.execute("INSERT INTO Instruments VALUES ('keyboard',5,500.0,'alesis')");
+         stat.execute("INSERT INTO Instruments VALUES ('drums',6,1500.0,'ludwig')");
+         stat.execute("INSERT INTO Instruments VALUES ('drums',7,400.0,'yamaha')");
+         ResultSet result = stat.executeQuery("SELECT * FROM Instruments");
+         sqlAccessLock.unlock();
+         return result;
+    }
+   
+    /*
+	 * Creates table Locations and returns table
+	 * @param Statement stat in order execute string based SQL string commands
+     * @return Locations table
+	 */
+	public ResultSet createLocations(Statement stat) throws Exception {	
+		 sqlAccessLock.lock();
+         stat.execute("CREATE TABLE Locations (locName CHAR(12),locNumber INTEGER,address CHAR(50))");
+         stat.execute("INSERT INTO Locations VALUES ('PNS',1,'Pensacola Florida')");
+         stat.execute("INSERT INTO Locations VALUES ('CLT',2,'Charlotte North Carolina')");
+         stat.execute("INSERT INTO Locations VALUES ('DFW',3,'Dallas Fort Worth Texas')");
+         ResultSet result = stat.executeQuery("SELECT * FROM Locations");
+         sqlAccessLock.unlock();
+         return result;
+    }
+   
+    /*
+	 * Creates table Inventory and returns table
+	 * @param Statement stat in order execute string based SQL string commands
+     * @return Inventory table
+	 */
+	public ResultSet createInventory(Statement stat) throws Exception {	
+		 sqlAccessLock.lock();
+         stat.execute("CREATE TABLE Inventory (iNumber INTEGER,lNumber INTEGER,quantity INTEGER)");
+         stat.execute("INSERT INTO Inventory VALUES (1,1,15)");
+         stat.execute("INSERT INTO Inventory VALUES (1,2,27)");
+         stat.execute("INSERT INTO Inventory VALUES (1,3,20)");
+         stat.execute("INSERT INTO Inventory VALUES (2,1,10)");
+         stat.execute("INSERT INTO Inventory VALUES (2,2,10)");
+         stat.execute("INSERT INTO Inventory VALUES (2,3,35)");
+         stat.execute("INSERT INTO Inventory VALUES (3,1,45)");
+         stat.execute("INSERT INTO Inventory VALUES (3,2,10)");
+         stat.execute("INSERT INTO Inventory VALUES (3,3,17)");
+         stat.execute("INSERT INTO Inventory VALUES (4,1,28)");
+         stat.execute("INSERT INTO Inventory VALUES (4,2,10)");
+         stat.execute("INSERT INTO Inventory VALUES (4,3,16)");
+         stat.execute("INSERT INTO Inventory VALUES (5,1,28)");
+         stat.execute("INSERT INTO Inventory VALUES (5,2,10)");
+         stat.execute("INSERT INTO Inventory VALUES (5,3,1)");
+         stat.execute("INSERT INTO Inventory VALUES (6,1,2)");
+         stat.execute("INSERT INTO Inventory VALUES (6,2,10)");
+         stat.execute("INSERT INTO Inventory VALUES (6,3,16)");
+         stat.execute("INSERT INTO Inventory VALUES (7,1,16)");
+         stat.execute("INSERT INTO Inventory VALUES (7,2,4)");
+         stat.execute("INSERT INTO Inventory VALUES (7,3,12)");     
+         ResultSet result = stat.executeQuery("SELECT * FROM Inventory");
+         sqlAccessLock.unlock();
+         return result;
+    }
+	
+	/**
+	 * Method runQueries will run the query string passed in and generate
+	 * a ResultSet Object that is then parsed in the runResult method to be returned
+	 * as a single string.
+	 * @param queryString
+	 * @return will return a result string based on the query
+	 * @throws Exception
+	 */
+	public String runQueries(String queryString) throws Exception {
+		sqlAccessLock.lock();
+		// Get the ResultSet object from executing the query string
+		ResultSet resultSet = stat.executeQuery(queryString);
+		sqlAccessLock.unlock();
+		return runResult(resultSet);
+	}
+	
+	/**
+	 * runResult will translate the ResultSet object to a nicely
+	 * formated return string to be sent to the client
+	 * @param result is the ResultSet object created in the 
+	 * runQueries method
+	 * @return returns a nicely formated string to be sent to the client
+	 * @throws Exception will be thrown if ResultSet can not be accessed
+	 */
+	public String runResult(ResultSet result) throws Exception {
+		StringBuffer queryResult = new StringBuffer();
+		//System.out.println("Translating return object of queries into a readable format");
+		ResultSetMetaData rsm = result.getMetaData();
+		int cols = rsm.getColumnCount();
+		while(result.next())
+		{
+			for(int i = 1; i <= cols; i++) {
+				queryResult.append(result.getString(i)+" ");
+				//System.out.print(result.getString(i)+" ");
+			}
+			queryResult.append(",");
+			//System.out.println("");      
+		}
+		
+		return queryResult.toString();
+
+	}
+
+}
---
+++ VaughanWelterServer/VaughanWelterServer/src/main/java/MusicRequest.java
@@ -0,0 +1,221 @@
+/****************************************************
+ 
+@author Dillon Vaughan, Anthony Welter
+File Name: MusicRequest.java
+COP4027	Project #: 5
+
+****************************************************/
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class MusicRequest implements Runnable {
+   private Socket s;
+   private Scanner in;
+   private PrintWriter out;
+   private InstrumentDB instrumentDB;
+   
+   public MusicRequest(Socket aSocket) {
+	   s = aSocket;
+   }
+   
+   /**
+    * Run is the method to be used from class Runnable
+    * run will set up the scanner and print writer, and then factor out
+    * Functionality 
+    */
+   public void run()
+   {
+      try
+      {
+         try
+         {	
+           // Try creating the db, drop it first, then create the db.
+        	instrumentDB = new InstrumentDB();
+    	   	// create scanners and print writer 
+            in = new Scanner(s.getInputStream());
+            out = new PrintWriter(s.getOutputStream());
+            doService();            
+         } catch (Exception e1) {
+			e1.printStackTrace();
+		}
+         finally
+         {	
+        	// When doService is done, close the connection
+            s.close();
+         }
+      }
+      catch (IOException exception)
+      {
+         exception.printStackTrace();
+      }
+   }
+   
+    /**
+	*  Executes all commands until the end of input.
+	*/
+	public void doService() throws IOException
+	{      
+	   while (true)
+	   {  
+	      if (!in.hasNext()) return;
+	      // Grabbing the string from the client
+	      String command = in.next();
+	      executeCommand(command);
+	   }
+	}
+	
+	
+	 /**
+	    Executes a single command.
+	    @param command the command to execute
+	 */
+	 public void executeCommand(String command) {
+		//System.out.println("Command at start of executeCommand is " + command);
+		
+		// setting the types of items to grab from commands
+		String type = "";
+		String brand = "";
+		double maxCost = 0;
+		int location = 0;
+		
+		// query response is the one line string that will be sent back to the client
+		// strings with multiple lines cannot be sent using the print writer 
+		String queryResponse = "NULL";
+		
+		// The main and only command of the program is RETRIEVE, once retrieve is 
+		// hit here, then four parameters will be collected below
+		if (command.equals("RETRIEVE")) {
+			type = in.next();
+			brand = in.next();
+			maxCost = in.nextDouble();
+			location = in.nextInt();
+			//System.out.println("I made it this far!");
+			//System.out.println("Type is: " + type + "\nBrand is: " + brand + "\nMax Cost is: " + maxCost + "\nLocation is: " + location);
+			
+			// Build the query string to search the database
+			String queryString = queryStringBuilder(type, brand, maxCost, location);
+			//System.out.println(queryString);
+			// execute the query, and in one move retrieve the response string from the database
+			queryResponse = executeQuery(queryString);
+			//System.out.println(queryResponse);
+		}
+		
+		 
+		//System.out.println("Here is what I got from the client");
+		
+		//System.out.println("Type is: " + type + "\nBrand is: " + brand + "\nMax Cost is: " + maxCost + "\nLocation is: " + location);
+		
+		// Sending the response of the query to the client in one string 
+		out.println(queryResponse);
+		out.flush();
+	 }
+	 
+	 /**
+	  * queryStringBuilder will compile the needed query string needed to search
+	  * for the desired parameters 
+	  * @param type is the type of instrument
+	  * @param brand is the brand of the instrument
+	  * @param maxCost is the max cost, results that are less than the max cost will be returned
+	  * @param location is the location in an integer representation 
+	  * @return will return a string of query string to be used to search the database later in the program
+	  */
+	 public String queryStringBuilder(String type, String brand, Double maxCost, int location) {
+		 StringBuffer queryString = new StringBuffer();
+		 
+		 boolean nonDefaultType = false;
+		 boolean nonDefaultBrand = false;
+		 boolean nonDefaultMaxCost = false;
+		 boolean nonDefaultLoction = false;
+		 boolean whereUsed = false;
+		 
+		 if (!type.contains("All")) {
+			 nonDefaultType = true;
+		 }
+		 if (!brand.contains("All")) {
+			 nonDefaultBrand = true;
+		 }
+		 if (maxCost != 0) {
+			 nonDefaultMaxCost = true;
+		 }
+		 if (location != 0) {
+			 nonDefaultLoction = true;
+		 }
+		 
+		 // query string is the query string object and always starts out the same way
+		 queryString.append("""
+				SELECT
+				    u.instName,
+				    u.descrip,
+				    u.cost,
+				    b.quantity,
+				    x.address
+				FROM
+				    Instruments u
+				JOIN
+				    Inventory b ON u.instNumber = b.iNumber
+				JOIN
+				    Locations x ON x.locNumber = b.lNumber
+		 		""");
+		 
+		 // These if and else if statements ensure that a good SQL query is built. Each 
+		 // parameter ensure that it is either the second value to be searched for, or the
+		 // first 	 
+		 if (nonDefaultType) {
+			 if (whereUsed) {
+				 queryString.append(" AND u.instName = '" + type + "' \n");
+			 } else {
+				 queryString.append("WHERE u.instName = '" + type + "' \n");
+				 whereUsed = true;
+			 }
+		 }
+		 
+		 if (nonDefaultBrand) {
+			 if (whereUsed) {
+				 queryString.append(" AND u.descrip = '" + brand + "' \n");
+			 } else {
+				 queryString.append("WHERE u.descrip = '" + brand + "' \n");
+				 whereUsed = true;
+			 }
+		 }
+		 
+		 if (nonDefaultMaxCost) {
+			 if (whereUsed) {
+				 queryString.append("AND u.cost <= " + maxCost + " \n");
+			 } else {
+				 queryString.append("WHERE u.cost <= " + maxCost + " \n");
+				 whereUsed = true;
+			 }
+		 }
+		 
+		 if (nonDefaultLoction) {
+			 if (whereUsed) {
+				 queryString.append("AND b.lNumber = " + location + " \n");
+			 } else {
+				 queryString.append("WHERE b.lNumber = " + location + " \n");
+				 whereUsed = true;
+			 }
+		 }
+		 
+		 return queryString.toString();
+	 }
+	 
+	 /**
+	  * executeQuery will actually search the database using the query string that is 
+	  * built by queryStringBuilder
+	  * @param queryString is the built query string from queryStringBuilder
+	  * @return will return a one line query response string of results 
+	  */
+	 public String executeQuery(String queryString) {
+		String response;
+		try {
+			response = instrumentDB.runQueries(queryString);
+			return response;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "Failed query";
+	 }
+}
